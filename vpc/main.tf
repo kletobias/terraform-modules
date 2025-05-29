a9848598878a161fa41defcccc327708ff96ae75
Tobias Klein <kle.tobias@googlemail.com>
2025-05-29 19:57:10 +0200

    chore(vpc/main.tf): add inline comments to clarify configuration options

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,20 +1,20 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.3.0" # Minimum required Terraform version
 }
 
 # This module uses the official Terraform AWS VPC module internally
 module "vpc_core" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.14"
+  source  = "terraform-aws-modules/vpc/aws" # Source of the VPC module
+  version = "~> 3.14" # Specify the version of the VPC module
 
-  name = var.vpc_name
-  cidr = var.vpc_cidr
+  name = var.vpc_name # Name for the VPC
+  cidr = var.vpc_cidr # CIDR block for the VPC
 
-  azs             = var.azs
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
+  azs             = var.azs # Availability Zones for the VPC
+  private_subnets = var.private_subnets # Private subnet CIDRs
+  public_subnets  = var.public_subnets # Public subnet CIDRs
 
-  enable_nat_gateway   = var.enable_nat_gateway
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_nat_gateway   = var.enable_nat_gateway # Enable NAT Gateway or not
+  enable_dns_hostnames = true # Enable DNS hostnames in the VPC
+  enable_dns_support   = true # Enable DNS support in the VPC
 }
