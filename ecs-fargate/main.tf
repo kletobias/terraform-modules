cd6cbcf2909d0c0200bc3f8e87aa6af9df11a012
Tobias Klein <kle.tobias@googlemail.com>
2025-05-30 11:43:29 +0200

    chore(main.tf): update ECS Fargate module source and version specification Replace git URL based module sources with terraform-aws-modules references and add explicit module version for consistency.

--- ecs-fargate/main.tf
+++ ecs-fargate/main.tf
@@ -1,5 +1,3 @@
-# terraform-modules/ecs-fargate/main.tf  (overwrite the file)
-
 terraform {
   required_version = ">= 1.3.0"
 }
@@ -8,24 +6,27 @@ terraform {
 # ECS cluster  #
 ################
 module "cluster" {
-  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-ecs.git//modules/cluster?ref=v4.1.3"
-  name    = "${var.service_name}-cluster"
+  source  = "terraform-aws-modules/ecs/aws//modules/cluster"
+  version = "~> 4.1"
+
+  name = "${var.service_name}-cluster"
 }
 
 ########################
 # Fargate service      #
 ########################
 module "service" {
-  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-ecs.git//modules/service?ref=v4.1.3"
+  source  = "terraform-aws-modules/ecs/aws//modules/service"
+  version = "~> 4.1"
 
-  name          = var.service_name
-  cluster_arn   = module.cluster.arn
-  launch_type   = "FARGATE"
+  name            = var.service_name
+  cluster_arn     = module.cluster.arn
+  launch_type     = "FARGATE"
 
-  cpu           = var.cpu
-  memory        = var.memory
-  desired_count = 0            # scale-to-zero by default
-  subnet_ids    = var.subnet_ids
+  cpu             = var.cpu
+  memory          = var.memory
+  desired_count   = 0              # scale-to-zero by default
+  subnet_ids      = var.subnet_ids
   assign_public_ip = false
 
   container_definitions = jsonencode(var.container_definitions)
