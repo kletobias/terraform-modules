18b4450134ad49d955b86c9f42307320567a4e91
Tobias Klein <kle.tobias@googlemail.com>
2025-05-30 12:15:04 +0200

    feat(main.tf): add ECS cluster module and update FARGATE service configuration Introduce a new ECS cluster module and update the service module configuration. Update includes: - Adding a new module "cluster" for ECS cluster setup. - Passing the cluster ARN to the service module. - Adjusting task size, networking, and scale-to-zero settings.

--- ecs-fargate/main.tf
+++ ecs-fargate/main.tf
@@ -1,28 +1,38 @@
-# terraform-modules/ecs-fargate/main.tf
 terraform {
   required_version = ">= 1.3.0"
 }
 
+################
+# ECS CLUSTER
+################
+module "cluster" {
+  source  = "terraform-aws-modules/ecs/aws//modules/cluster"
+  version = "~> 4.1"
+
+  name = "${var.service_name}-cluster"
+}
+
+############################
+# FARGATE SERVICE
+############################
 module "service" {
   source  = "terraform-aws-modules/ecs/aws//modules/service"
   version = "~> 4.1"
 
-  ###########################
-  # Cluster & networking
-  ###########################
-  create_cluster  = true
-  cluster_name    = "${var.service_name}-cluster"
-  launch_type     = "FARGATE"
+  # ---- mandatory ----
+  name        = var.service_name
+  cluster_arn = module.cluster.arn
+  launch_type = "FARGATE"
+
+  # ---- networking ----
   subnets         = var.subnet_ids
   assign_public_ip = false
 
-  ###########################
-  # Task / service settings
-  ###########################
-  name          = var.service_name
+  # ---- task size & scale ----
   cpu           = var.cpu
   memory        = var.memory
-  desired_count = 0                           # scale-to-zero
+  desired_count = 0           # scale-to-zero by default
 
+  # ---- containers ----
   container_definitions = jsonencode(var.container_definitions)
 }
