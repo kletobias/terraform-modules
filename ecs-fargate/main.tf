e9f966425109c3db956377c79aa99fc742ecc38c
Tobias Klein <kle.tobias@googlemail.com>
2025-05-30 11:09:22 +0200

    refactor(main.tf): split ECS Fargate configuration into cluster and service modules

--- ecs-fargate/main.tf
+++ ecs-fargate/main.tf
@@ -1,15 +1,37 @@
+# terraform-modules/ecs-fargate/main.tf
 terraform {
   required_version = ">= 1.3.0"
 }
 
-module "ecs_fargate" {
-  source  = "terraform-aws-modules/ecs/aws//modules/fargate-service"
+##########
+# CLUSTER
+##########
+module "cluster" {
+  source  = "terraform-aws-modules/ecs/aws//modules/cluster"
   version = "~> 4.1"
 
-  service_name          = var.service_name
-  cpu                   = var.cpu
-  memory                = var.memory
-  vpc_id                = var.vpc_id
-  subnets               = var.subnet_ids
-  container_definitions = jsonencode(var.container_definitions)
+  name = "${var.service_name}-cluster"
+}
+
+#####################
+# FARGATE SERVICE
+#####################
+module "service" {
+  source  = "terraform-aws-modules/ecs/aws//modules/service"
+  version = "~> 4.1"
+
+  name            = var.service_name
+  cluster_arn     = module.cluster.arn
+  launch_type     = "FARGATE"
+
+  cpu             = var.cpu
+  memory          = var.memory
+  desired_count   = 0                        # scale-to-zero by default
+  subnet_ids      = var.subnet_ids
+  assign_public_ip = false
+
+  task_exec_iam_role_arn = null             # use default if you don’t pass one
+  task_role_arn          = null
+
+  container_definitions  = jsonencode(var.container_definitions)
 }
