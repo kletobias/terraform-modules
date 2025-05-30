7cf1419457585c511796ed23d4f0831fc06cf1ba
Tobias Klein <kle.tobias@googlemail.com>
2025-05-30 13:23:26 +0200

    chore(main.tf): update formatting for ECS modules This change removes redundant comments and adjusts indentation for consistency.

--- ecs-fargate/main.tf
+++ ecs-fargate/main.tf
@@ -2,36 +2,23 @@ terraform {
   required_version = ">= 1.3.0"
 }
 
-################
-# ECS CLUSTER
-################
 module "cluster" {
-  # git URL + sub-path
   source  = "git::https://github.com/terraform-aws-modules/terraform-aws-ecs.git//modules/cluster?ref=v4.1.3"
   name    = "${var.service_name}-cluster"
 }
 
-########################
-# FARGATE SERVICE
-########################
 module "service" {
-  # git URL + sub-path
   source  = "git::https://github.com/terraform-aws-modules/terraform-aws-ecs.git//modules/service?ref=v4.1.3"
 
-  # ----- mandatory -----
-  name        = var.service_name
-  cluster_arn = module.cluster.arn
-  launch_type = "FARGATE"
+  name            = var.service_name
+  cluster_arn     = module.cluster.arn
+  launch_type     = "FARGATE"
 
-  # ----- networking -----
   subnets          = var.subnet_ids
   assign_public_ip = false
 
-  # ----- task size & scale -----
   cpu           = var.cpu
   memory        = var.memory
-  desired_count = 0            # scale-to-zero
-
-  # ----- containers -----
+  desired_count = 0
   container_definitions = jsonencode(var.container_definitions)
 }
