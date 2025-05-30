b11c431788943a6d7c3f25eb7df1db8e877cae9c
Tobias Klein <kle.tobias@googlemail.com>
2025-05-30 10:40:56 +0200

    style(vpc/main.tf): update alignment for improved readability

--- vpc/main.tf
+++ vpc/main.tf
@@ -5,16 +5,16 @@ terraform {
 # This module uses the official Terraform AWS VPC module internally
 module "vpc_core" {
   source  = "terraform-aws-modules/vpc/aws" # Source of the VPC module
-  version = "~> 3.14" # Specify the version of the VPC module
+  version = "~> 3.14"                       # Specify the version of the VPC module
 
   name = var.vpc_name # Name for the VPC
   cidr = var.vpc_cidr # CIDR block for the VPC
 
-  azs             = var.azs # Availability Zones for the VPC
+  azs             = var.azs             # Availability Zones for the VPC
   private_subnets = var.private_subnets # Private subnet CIDRs
-  public_subnets  = var.public_subnets # Public subnet CIDRs
+  public_subnets  = var.public_subnets  # Public subnet CIDRs
 
   enable_nat_gateway   = var.enable_nat_gateway # Enable NAT Gateway or not
-  enable_dns_hostnames = true # Enable DNS hostnames in the VPC
-  enable_dns_support   = true # Enable DNS support in the VPC
+  enable_dns_hostnames = true                   # Enable DNS hostnames in the VPC
+  enable_dns_support   = true                   # Enable DNS support in the VPC
 }
