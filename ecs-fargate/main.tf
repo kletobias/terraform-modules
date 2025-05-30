085d6d9869aa7accea145fae54eb6cf8af4ef6fc
Tobias Klein <kle.tobias@googlemail.com>
2025-05-30 11:27:23 +0200

    chore(main.tf): update module source to github for ECS cluster and service This change replaces the previous module source with the GitHub-based source for both the ECS cluster and Fargate service.

--- ecs-fargate/main.tf
+++ ecs-fargate/main.tf
@@ -1,37 +1,32 @@
-# terraform-modules/ecs-fargate/main.tf
+# terraform-modules/ecs-fargate/main.tf  (overwrite the file)
+
 terraform {
   required_version = ">= 1.3.0"
 }
 
-##########
-# CLUSTER
-##########
+################
+# ECS cluster  #
+################
 module "cluster" {
-  source  = "terraform-aws-modules/ecs/aws//modules/cluster"
-  version = "~> 4.1"
-
-  name = "${var.service_name}-cluster"
+  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-ecs.git//modules/cluster?ref=v4.1.3"
+  name    = "${var.service_name}-cluster"
 }
 
-#####################
-# FARGATE SERVICE
-#####################
+########################
+# Fargate service      #
+########################
 module "service" {
-  source  = "terraform-aws-modules/ecs/aws//modules/service"
-  version = "~> 4.1"
+  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-ecs.git//modules/service?ref=v4.1.3"
 
-  name            = var.service_name
-  cluster_arn     = module.cluster.arn
-  launch_type     = "FARGATE"
+  name          = var.service_name
+  cluster_arn   = module.cluster.arn
+  launch_type   = "FARGATE"
 
-  cpu             = var.cpu
-  memory          = var.memory
-  desired_count   = 0                        # scale-to-zero by default
-  subnet_ids      = var.subnet_ids
+  cpu           = var.cpu
+  memory        = var.memory
+  desired_count = 0            # scale-to-zero by default
+  subnet_ids    = var.subnet_ids
   assign_public_ip = false
 
-  task_exec_iam_role_arn = null             # use default if you don’t pass one
-  task_role_arn          = null
-
-  container_definitions  = jsonencode(var.container_definitions)
+  container_definitions = jsonencode(var.container_definitions)
 }
