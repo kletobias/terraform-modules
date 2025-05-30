7e59ace4190c7479e839da4b8e18a4573d0d98b7
Tobias Klein <kle.tobias@googlemail.com>
2025-05-30 11:58:05 +0200

    refactor(main.tf): consolidate cluster configuration into service module

--- ecs-fargate/main.tf
+++ ecs-fargate/main.tf
@@ -1,33 +1,28 @@
+# terraform-modules/ecs-fargate/main.tf
 terraform {
   required_version = ">= 1.3.0"
 }
 
-################
-# ECS cluster  #
-################
-module "cluster" {
-  source  = "terraform-aws-modules/ecs/aws//modules/cluster"
-  version = "~> 4.1"
-
-  name = "${var.service_name}-cluster"
-}
-
-########################
-# Fargate service      #
-########################
 module "service" {
   source  = "terraform-aws-modules/ecs/aws//modules/service"
   version = "~> 4.1"
 
-  name            = var.service_name
-  cluster_arn     = module.cluster.arn
+  ###########################
+  # Cluster & networking
+  ###########################
+  create_cluster  = true
+  cluster_name    = "${var.service_name}-cluster"
   launch_type     = "FARGATE"
-
-  cpu             = var.cpu
-  memory          = var.memory
-  desired_count   = 0              # scale-to-zero by default
-  subnet_ids      = var.subnet_ids
+  subnets         = var.subnet_ids
   assign_public_ip = false
+
+  ###########################
+  # Task / service settings
+  ###########################
+  name          = var.service_name
+  cpu           = var.cpu
+  memory        = var.memory
+  desired_count = 0                           # scale-to-zero
 
   container_definitions = jsonencode(var.container_definitions)
 }
